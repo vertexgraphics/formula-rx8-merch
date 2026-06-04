10afb546d977e12120c5a470d566f941d7d75e2f
vertexgraphics <sales@vertexgraphics.com.au>
2026-06-04 22:31:03 +1000

    Add Google Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import ReactGA from "react-ga4";
-ReactGA.initialize("G-5BMPFK6GKW");
-ReactGA.send("pageview");
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

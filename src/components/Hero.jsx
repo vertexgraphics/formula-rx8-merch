a67a42c101e14a25193d953e903fc8634f023999
vertexgraphics <sales@vertexgraphics.com.au>
2026-06-05 10:19:41 +1000

    Update hero banner

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,10 +2,7 @@ function Hero() {
   return (
     <section className="hero">
       <div className="hero-content">
-        <h1>
-          <span className="hero-white">Formula RX8</span>
-          <span className="hero-red">Fan Merch</span>
-        </h1>
+
       </div>
     </section>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -37,16 +37,7 @@ function Home() {
   return (
     <main className="page">
       <section className="hero">
-        <div className="hero-content">
-          <h1>
-            <span className="hero-white">Formula RX8</span>
-            <span className="hero-red">Fan Merch</span>
-          </h1>
 
-          <p>
-
-          </p>
-        </div>
       </section>
 
       <Navbar />

e4ad8483f72466f209a558fdfffae4bfc907817a
vertexgraphics <sales@vertexgraphics.com.au>
2026-06-09 21:48:25 +1000

    Add Beanies

--- src/pages/Beanies.jsx
+++ src/pages/Beanies.jsx
@@ -3,16 +3,46 @@ import CategoryPage from "../components/CategoryPage";
 function Beanies() {
   const products = [
     {
-      name: "Classic Logo Beanie",
-      price: "$24.95",
-      image: "/images/products/beanie-logo.jpg",
-      description: "Embroidered Formula RX8 logo beanie.",
+      name: "Formula RX8 Beanie - Red/Black",
+      price: "$25.00",
+      image: "/images/products/red-black-beanie.png",
+      description: "Formula RX8 Beanie - Red/Black",
+      link: "https://vertexgraphics.com.au/products/formula-rx8-beanie-red-black",
     },
     {
-      name: "Trackside Beanie",
-      price: "$24.95",
-      image: "/images/products/beanie-trackside.jpg",
-      description: "Perfect for early race mornings and winter rounds.",
+      name: "Formula RX8 Beanie - Red/White",
+      price: "$25.00",
+      image: "/images/products/red-white-beanie.png",
+      description: "Formula RX8 Beanie - Red/White",
+      link: "https://vertexgraphics.com.au/products/formula-rx8-beanie-red-white",
+    },
+    {
+      name: "Formula RX8 Beanie - Red/Gold",
+      price: "$25.00",
+      image: "/images/products/red-gold-beanie.png",
+      description: "Formula RX8 Beanie - Red/Gold",
+      link: "https://vertexgraphics.com.au/products/formula-rx8-beanie-red-gold",
+    },
+        {
+      name: "Formula RX8 Beanie - Black/Black",
+      price: "$25.00",
+      image: "/images/products/black-black-beanie.png",
+      description: "Formula RX8 Beanie - Black/Black",
+      link: "https://vertexgraphics.com.au/products/formula-rx8-beanie-black-black",
+    },
+    {
+      name: "Formula RX8 Beanie - Black/White",
+      price: "$25.00",
+      image: "/images/products/black-white-beanie.png",
+      description: "Formula RX8 Beanie - Black/White",
+      link: "https://vertexgraphics.com.au/products/formula-rx8-beanie-black-white",
+    },
+        {
+      name: "Formula RX8 Beanie - Black/Gold",
+      price: "$25.00",
+      image: "/images/products/black-gold-beanie.png",
+      description: "Formula RX8 Beanie - Black/Gold",
+      link: "https://vertexgraphics.com.au/products/formula-rx8-beanie-black-gold",
     },
   ];
 
